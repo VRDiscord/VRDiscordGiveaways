d2b5c7d21499a6136b49ea02131345351536f39f
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-08-22 08:30:48 +0200

    add logs to getKey

--- src/buttons/getKey.ts
+++ src/buttons/getKey.ts
@@ -13,10 +13,12 @@ export default class Test extends Button {
     async run(ctx: ButtonContext): Promise<any> {
         let check = await ctx.sql.query(`SELECT * FROM freekeys WHERE id='${ctx.interaction.message.id}' AND user_id='${ctx.interaction.user.id}' LIMIT 1`)
         if(check.rowCount) return ctx.reply({content: `You already got a key: \`${check.rows[0].prize}\``, ephemeral: true})
-        let prize = await ctx.sql.query(`SELECT * FROM freekeys WHERE id='${ctx.interaction.message.id}' AND user_id IS NULL LIMIT 1`)
-        if(!prize.rowCount) return ctx.error("All keys have been handed out already")
-        await ctx.sql.query(`UPDATE freekeys SET user_id='${ctx.interaction.user.id}' WHERE prize=$1 AND id='${ctx.interaction.message.id}'`, [prize.rows[0].prize])
+        let prizequery = await ctx.sql.query(`SELECT * FROM freekeys WHERE id='${ctx.interaction.message.id}' AND user_id IS NULL LIMIT 1`).catch(console.error)
+        if(!prizequery?.rowCount) return ctx.error("All keys have been handed out already")
+        const prize = prizequery.rows[0]!
+        await ctx.sql.query(`UPDATE freekeys SET user_id='${ctx.interaction.user.id}' WHERE prize=$1 AND id='${ctx.interaction.message.id}'`, [prize.prize])
 
-        ctx.reply({content: `Here's a key: \`${prize.rows[0].prize}\``, ephemeral: true})
+        ctx.log(`${ctx.interaction.user.username} (\`${ctx.interaction.user.id}\`) received key \`${prize.prize}\` from key handout \`${ctx.interaction.message.id}\``, [{type: 1, components: [{type: 2, label: "View Message", style: 5, url: `https://discord.com/channels/${process.env["GUILD_ID"]}/${prize.channel_id}/${ctx.interaction.message.id}`}]}])
+        ctx.reply({content: `Here's a key: \`${prize.prize}\``, ephemeral: true})
     }
 }
